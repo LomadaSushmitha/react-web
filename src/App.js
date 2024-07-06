ed3473f944f198f27786b24f4e949963f65b40a2
=LomadaSushmitha <=sushmitha3804@gmail.com>
2024-07-06 10:02:32 +0530

    Intialized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './components/home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Home/>
+      <NewsComponents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/video/Video.js
@@ -0,0 +1,54 @@
+import React ,{ useState, useEffect } from "react";
+
+function Video() {
+
+  const  [Videos,setVideos] = useState([])
+   
+  useEffect(() => {
+    fetch("videoData.json")
+    .then(response => response.json())
+    .then(data => setVideos(data))
+    .catch(error => console.error("problem with fetching data!!",error))
+  
+
+  }, [])
+
+  
+    
+  return(
+    
+       <div clasName="container">
+         <div ClassName="row">
+           {
+             Videos.map((videos,key)) => (
+               <div key={key} className="col-md-4">
+                 <a href={`https://youtube.com//watch?v=${video.img_id}`}>
+                   <div classname="card">
+                     <img
+                     src={`https://img.youtube.com/vi/${video.img_id}/0.jpg`}
+                     className="img_thumbnail"
+                     alt={video.title}
+                     />
+                     <div classname="card-body">
+                       <h5 className="card-title">{video.title}</h5>
+                       <p className="card-text">{video.channelName}</p>
+                       <small>
+                         {{video.viewsCount} views - {video.likesCount} likes-
+                         {video.duration}m
+                          </small>
+                        </div>
+                      </div>
+                    </a>    
+
+          </div>
+             )
+           }
+         </div>
+
+       </div>
+  );
+}
+
+export default Video
+
+//so it is running for everyone now
